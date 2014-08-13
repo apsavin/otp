871e00be53dc25cfb2e2e8659c96396ba7beff81
Alexander Savin <box.for.postman@gmail.com>
2014-08-13 22:18:10 +0400

    More JSDOC for FlightsController

--- api/controllers/FlightsController.js
+++ api/controllers/FlightsController.js
@@ -4,11 +4,25 @@ module.exports = {
 
     _config: { blueprints: { rest: false, actions: false } },
 
+    /**
+     * @param {IncomingMessage} req
+     * @param {OutgoingMessage} res
+     */
     index: function (req, res) {
 
         var company = req.param('company');
 
-        FlightsService.getFlights(req.param('airport'), req.param('scheduleType'), company ? 12 : 4)
+        /**
+         * @const {number} count of hours
+         */
+        var SHIFT = 4;
+
+        /**
+         * @const {number} count of hours
+         */
+        var COMPANY_SHIFT = 12;
+
+        FlightsService.getFlights(req.param('airport'), req.param('scheduleType'), company ? COMPANY_SHIFT : SHIFT)
             .then(function (result) {
                 if (company && result.data) {
                     result.data = result.data.filter(function (row) {
